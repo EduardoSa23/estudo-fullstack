7588f0d92929b3a1e97b7494d3d4520533ca8b77
Eduardo Sá <111080797+EduardoSa23@users.noreply.github.com>
2026-05-08 13:58:05 -0300

    refactor: modelo webpack configurado

---
+++ aula94/src/index.js
@@ -0,0 +1,3 @@
+import { nome, sobrenome, idade, soma } from './modulo1'
+console.log(`${nome} ${sobrenome} tem ${idade} anos`)
+console.log(soma(5, 8))
---
+++ aula94/src/modulo1.js
@@ -0,0 +1,9 @@
+const nome = 'Eduardo'
+const sobrenome = 'Sá'
+const idade = 28
+
+function soma (x, y) {
+  return x + y
+}
+
+export { nome, sobrenome, idade, soma }
